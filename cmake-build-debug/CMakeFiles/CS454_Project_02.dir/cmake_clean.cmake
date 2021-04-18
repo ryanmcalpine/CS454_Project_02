c3140038858cf4d86b0240762005c9690d564a24
ryanmcalpine <57776028+ryanmcalpine@users.noreply.github.com>
2021-04-17 22:42:47 -0700

    Initial Set-up

---
+++ cmake-build-debug/CMakeFiles/CS454_Project_02.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CS454_Project_02.dir/main.cpp.obj"
+  "CS454_Project_02.exe"
+  "CS454_Project_02.exe.manifest"
+  "CS454_Project_02.pdb"
+  "libCS454_Project_02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CS454_Project_02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
